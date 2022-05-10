6bc1c85e341c46ed5ca38126c1dbe3267c4f49cc
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-05-10 10:42:02 +0200

    Adds css to the view

--- frontend/src/Views/PrincipalView/PrincipalView.jsx
+++ frontend/src/Views/PrincipalView/PrincipalView.jsx
@@ -1,14 +1,15 @@
+import './PrincipalView.css';
 import ProfileCard from "../../components/ProfileCard/ProfileCard";
 
 function PrincipalView () {
     return(
-        <>
+        <div className="principalContainer">
             <h1>This is a Principal view</h1>
             <ProfileCard/>
             <ProfileCard/>
             <ProfileCard/>
             <ProfileCard/>
-        </>
+        </div>
     );
 }
 export default PrincipalView;
