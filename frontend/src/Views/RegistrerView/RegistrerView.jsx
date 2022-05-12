ecc386f097afc3acaf78bf5fdae0a44b607c1c23
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-05-12 10:57:59 +0200

    Base to the RegistrerView

--- frontend/src/Views/RegistrerView/RegistrerView.jsx
+++ frontend/src/Views/RegistrerView/RegistrerView.jsx
@@ -4,6 +4,42 @@ function RegistrerView() {
     return(
         <div className='registrerContainer'>
             <h1>This is the RegistrerView</h1>
+            <label> 
+                Nombre de perfil
+                <input id="nameProfile" type="text"/>
+            </label>
+            <label>
+                Nombre
+                <input id="nameUser" type="text"/>
+            </label>
+            <label>
+                Contraseña
+                <input type="password" />
+            </label>
+            <label>
+                Ciudad
+                <input type="text"/>
+            </label>
+            <label>
+                Intereses
+                <input type="text"/>
+            </label>
+            <label>
+                Edad
+                <input type="text"/>
+            </label>
+            <label>
+                Descripción
+                <input type="text"/>
+            </label>
+            <label>
+                Correo electrónico
+                <input type="text"/>
+            </label>
+            <label>
+                Foto
+                <input type="text"/>
+            </label>
         </div>
     );
 }
