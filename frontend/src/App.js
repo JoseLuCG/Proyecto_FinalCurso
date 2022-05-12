c4b178e193ba0d006792ecb324bf37955baeb199
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-05-12 09:51:11 +0200

    Create the the view <StartView>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import PrincipalView from './Views/PrincipalView/PrincipalView';
+import StartView from './Views/StartView/StartView';
 
 function App() {
   return (
     <>
+      <StartView/>
       <PrincipalView/>
     </>
   );
---
+++ frontend/src/Views/StartView/StartView.jsx
@@ -0,0 +1,9 @@
+function StartView () {
+    return(
+        <div>
+            <h1>This is the startView</h1>
+            <p>Ey, listen!</p>
+        </div>
+    );
+}
+export default StartView;
