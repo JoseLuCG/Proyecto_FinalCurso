c46fe3fff692a7655229952d077d4f057959abd9
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-05-12 10:41:20 +0200

    Create the view <RegistrerView>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import PrincipalView from './Views/PrincipalView/PrincipalView';
+import RegistrerView from './Views/RegistrerView/RegistrerView';
 import StartView from './Views/StartView/StartView';
 
 function App() {
   return (
     <>
       <StartView/>
+      <RegistrerView/>
       <PrincipalView/>
     </>
   );
---
+++ frontend/src/Views/RegistrerView/RegistrerView.jsx
@@ -0,0 +1,10 @@
+import './RegistrerView.css';
+
+function RegistrerView() {
+    return(
+        <div className='registrerContainer'>
+            <h1>This is the RegistrerView</h1>
+        </div>
+    );
+}
+export default RegistrerView;
