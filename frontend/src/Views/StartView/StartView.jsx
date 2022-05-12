e86db7dcdcf1fcc3d72a87895861a64c090c96ca
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-05-12 10:13:33 +0200

    Update the Css of the <StartView>

--- frontend/src/Views/StartView/StartView.jsx
+++ frontend/src/Views/StartView/StartView.jsx
@@ -4,8 +4,13 @@ function StartView () {
     return(
         <div className='logingContainer'>
             <h1>Login view</h1>
-            <input type="text" />
-            <input type="password" />
+            <img className='LogoApp' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtepaT1fXQVI9_pyOM34JaAx7aASFWzfmyQg&usqp=CAU" alt="Logo imagen" />
+            <input type="text" placeholder='Nombre de usuario o email'/>
+            <input type="password" placeholder='Contraseña'/>
+            <div>
+                <button>Entrar</button>
+                <button>Registrarse</button>
+            </div>
         </div>
     );
 }
