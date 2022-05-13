eddf40e731ea49a789a69de28e6cf9be9da9ea99
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-05-13 09:52:54 +0200

    Modify the app.js to prove the <OptionsContainer>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import OptionsContainer from './components/OptionsContainer/OptionsContainer';
 import PrincipalView from './Views/PrincipalView/PrincipalView';
 import RegistrerView from './Views/RegistrerView/RegistrerView';
 import StartView from './Views/StartView/StartView';
@@ -6,6 +7,7 @@ import StartView from './Views/StartView/StartView';
 function App() {
   return (
     <>
+      <OptionsContainer/>
       <StartView/>
       <RegistrerView/>
       <PrincipalView/>
