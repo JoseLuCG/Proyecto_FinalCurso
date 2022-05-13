fb3bd9b70d32ebc205fd3cec4df89bf3b03cd324
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-05-13 09:49:53 +0200

    Create the component <CircleButton>

---
+++ frontend/src/components/CircleButton/CircleButton.jsx
@@ -0,0 +1,10 @@
+import './CircleButton.css';
+
+function CircleButton () {
+    return(
+        <button id='circleButton'>
+            back
+        </button>
+    );
+}
+export default CircleButton;
