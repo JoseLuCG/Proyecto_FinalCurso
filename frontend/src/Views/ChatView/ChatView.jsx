f6ef80e42eac73789551a667d30a57169617a25e
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-05-13 10:24:46 +0200

    Create the view <ChatView>

---
+++ frontend/src/Views/ChatView/ChatView.jsx
@@ -0,0 +1,10 @@
+import './ChatView.css';
+
+function ChatView () {
+    return(
+        <div className='chatContainer'>
+            <h2>This is the chat view</h2>
+        </div>
+    );
+}
+export default ChatView;
