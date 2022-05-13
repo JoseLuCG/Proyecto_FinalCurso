03e3080dd5d2dbfdc342584deb16702992c9f130
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-05-13 10:24:02 +0200

    Create the component <Message>

---
+++ frontend/src/components/Message/Message.jsx
@@ -0,0 +1,11 @@
+import './Message.css';
+
+function Message () {
+    return (
+        <>
+        <p>This is the component message</p>
+        <p className='message'>This is a message</p>
+        </>
+    );
+}
+export default Message;
