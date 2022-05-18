00525be7e24da08bfb6fd995111133854ce759fe
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-05-18 06:57:37 +0200

    Create the states

--- frontend/src/Views/RegistrerView/RegistrerView.jsx
+++ frontend/src/Views/RegistrerView/RegistrerView.jsx
@@ -1,8 +1,21 @@
+import { useState } from 'react';
 import CircleButton from '../../components/CircleButton/CircleButton';
 import SaveButton from '../../components/SaveButton/SaveButton';
 import './RegistrerView.css';
 
 function RegistrerView() {
+    //States:
+    const [ nameProfile, setNameProfile ] = useState("");
+    const [ nameUser, setNameUser ] = useState("");
+    const [ password, setPassword ] = useState("");
+    const [ location, setLocation ] = useState("");
+    const [ interest, setInterest ] = useState([]);
+    const [ age , setAge ] = useState (0);
+    const [ description, setDescription ] = useState("");
+    const [ email, setEmail ] = useState("");
+    const [ photo, setPhoto ] = useState("");
+
+    
     return(
         <div className='registrerContainer'>
             <h1>This is the RegistrerView</h1>
