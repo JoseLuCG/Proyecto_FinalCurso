7c974087a2e61dac0535fb43061f78f4d1766580
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-05-18 11:00:21 +0200

    Created the controlers of express

---
+++ backend/src/controlers.mjs
@@ -0,0 +1,67 @@
+import { db } from "./data/database.mjs";
+
+export function firstControler(req, res) {
+    try {
+        const {
+            nameProfile,
+            nameUser,
+            password,
+            location,
+            interest,
+            age,
+            description,
+            email
+        } = req.body;
+        const sql = `
+        INSERT INTO users(
+            nameProfile,
+            nameUser,
+            password,
+            location,
+            interest,
+            age,
+            description,
+            email)
+            VALUES(
+                "${nameProfile}",
+                "${nameUser}",
+                "${password}",
+                "${location}",
+                "${interest}",
+                "${age}",
+                "${description}",
+                "${email}"
+            )
+
+        `;
+
+        db.run(sql, (err) => {
+            if (err) {
+                console.error(err);
+                res.sendStatus(500);
+            } else {
+                res.sendStatus(201)
+            }
+        });
+    } catch (err) {
+        console.error(err);
+        res.sendStatus(500);
+    }
+}
+
+export function getUsersControler(req, res){
+    try{
+        const sql = `SELECT * FROM users`;
+        db.all(sql, (err, data)=>{
+            if (err){
+                console.error(err);
+                res.sendStatus(500);
+            } else {
+                res.json(data);
+            }
+        })
+    }catch (err) {
+        console.error(err);
+        res.sendStatus(500);
+    }
+}
