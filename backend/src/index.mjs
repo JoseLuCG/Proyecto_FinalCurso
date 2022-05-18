7d48011d4229c87d5c9d65a12cf6d646357d7011
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-05-18 11:03:03 +0200

    Create the express instance

---
+++ backend/src/index.mjs
@@ -0,0 +1,19 @@
+import express from "express";
+import { firstControler, getUsersControler } from "./controlers.mjs";
+import { PORT } from "./models/defines.mjs";
+
+//Create the instances:
+const app = express();
+const jsonParser = express.json();
+
+// Endpoints of the API:
+try{
+    app.post("/singup/",jsonParser, firstControler);
+    app.get("/users/", getUsersControler);
+
+    app.listen(PORT, ()=>{
+        console.log("Express Running...")
+    });
+}catch (err){
+    console.log(err);
+}
