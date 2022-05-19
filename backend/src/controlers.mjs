be956aed52637b79f48582de31f699b37afad174
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-05-19 07:11:45 +0200

    Adds the documentation in the <controlers.mjs>

--- backend/src/controlers.mjs
+++ backend/src/controlers.mjs
@@ -1,5 +1,13 @@
 import { db } from "./data/database.mjs";
 
+/**
+ * Obtain the object `user` and realize a query with
+ * the properties of them. Introduce the object data
+ * in the database with your data and create a a new
+ * row in the database.
+ * @param {*} req 
+ * @param {*} res 
+ */
 export function singupControler(req, res) {
     try {
         const {
@@ -49,6 +57,11 @@ export function singupControler(req, res) {
     }
 }
 
+/**
+ * Realize a query at the database obtains all users.
+ * @param {*} req 
+ * @param {*} res 
+ */
 export function getUsersControler(req, res){
     try{
         const sql = `SELECT * FROM users`;
