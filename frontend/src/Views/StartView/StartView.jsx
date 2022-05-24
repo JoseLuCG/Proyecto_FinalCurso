44b0c767a99f2811eea921a4a04f665ce00e887d
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-05-24 06:52:44 +0200

    Import the <changeValueFactory> and add functional states

--- frontend/src/Views/StartView/StartView.jsx
+++ frontend/src/Views/StartView/StartView.jsx
@@ -1,12 +1,21 @@
+import { useState } from 'react';
+import { changeValueFactory } from '../../apptools.mjs';
 import './StartView.css';
 
 function StartView () {
+    //States:
+    const [ userInfo , setUserInfo ] = useState("");
+    const [ password, setPassword ] = useState("");
+
+    //Handlers:
+    const userInfoChangeHandler = changeValueFactory(setUserInfo);
+    const passwordChangeHandler = changeValueFactory(setPassword);
     return(
         <div className='logingContainer'>
             <h1>Login view</h1>
             <img className='LogoApp' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtepaT1fXQVI9_pyOM34JaAx7aASFWzfmyQg&usqp=CAU" alt="Logo imagen" />
-            <input type="text" placeholder='Nombre de usuario o email'/>
-            <input type="password" placeholder='Contraseña'/>
+            <input onChange={userInfoChangeHandler} type="text" placeholder='Nombre de usuario o email'/>
+            <input onChange={passwordChangeHandler} type="password" placeholder='Contraseña'/>
             <div>
                 <button>Entrar</button>
                 <button>Registrarse</button>
