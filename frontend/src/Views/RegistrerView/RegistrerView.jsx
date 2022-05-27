6e45d4e81c99e9cc4210cffe13e222158a407c51
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-05-27 07:18:51 +0200

    Pass the states in the <RegistrerView> at the <ProfileCard>

--- frontend/src/Views/RegistrerView/RegistrerView.jsx
+++ frontend/src/Views/RegistrerView/RegistrerView.jsx
@@ -1,53 +1,9 @@
-import { useEffect, useState, useContext } from 'react';
-import { Context } from '../../storage/SharedStorage.jsx';
-import { changeValueFactory } from '../../tools/apptools.mjs';
 import CircleButton from '../../components/CircleButton/CircleButton';
 import SaveButton from '../../components/SaveButton/SaveButton';
 import './RegistrerView.css';
 import ProfileCard from '../../components/ProfileCard/ProfileCard.jsx';
 
 function RegistrerView() {
-    //----------States:----------
-    const [ store, setStore ] = useContext(Context);
-
-    const [ nameProfile, setNameProfile ] = useState("");
-    const [ nameUser, setNameUser ] = useState("");
-    const [ password, setPassword ] = useState("");
-    const [ location, setLocation ] = useState("");
-    const [ interest, setInterest ] = useState([]);
-    const [ age , setAge ] = useState (0);
-    const [ description, setDescription ] = useState("");
-    const [ email, setEmail ] = useState("");
-    const [ photo, setPhoto ] = useState("");
-    
-    //----------Handlers:----------
-
-    const nameProfileChangeHandler = changeValueFactory(setNameProfile);
-    const nameChangeHandler = changeValueFactory(setNameUser);
-    const passwordChangeHandler = changeValueFactory(setPassword);
-    const locationChangeHandler = changeValueFactory(setLocation);
-    const interestChangeHandler = changeValueFactory(setInterest);
-    const ageChangeHandler = changeValueFactory(setAge);
-    const descriptionChangeHandler = changeValueFactory(setDescription);
-    const emailChangeHandler = changeValueFactory(setEmail);
-    const photoChangeHandler = changeValueFactory(setPhoto);
-
-    //----------UseEfects:----------
-
-    //----------Functions:----------
-    function saveData () {
-        const newStore = {...store};
-        newStore.nameProfile = nameProfile;
-        newStore.nameUser = nameUser;
-        newStore.password = password;
-        newStore.location = location;
-        newStore.interest = interest;
-        newStore.age = age;
-        newStore.description = description;
-        newStore.email = email;
-        newStore.photo = photo;
-        setStore(newStore);
-    }
 
     return(
         <div className='registrerContainer'>
