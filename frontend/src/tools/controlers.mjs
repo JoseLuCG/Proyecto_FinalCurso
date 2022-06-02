6241d4209c6b2315ed010e177877f513e289df75
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-06-02 10:04:04 +0200

    Not completed. instancing the constants

--- frontend/src/tools/controlers.mjs
+++ frontend/src/tools/controlers.mjs
@@ -1,15 +1,38 @@
 import { URL } from "./defines.mjs"
+//----------Constatnts----------
+const jsonData = JSON.stringify(data);
+const params = {
+    method: 'POST',
+    body: jsonData,
+    headers: {
+        "Content-Type": "application/json"
+    }
+};
 
+//----------Functions----------
 export async function registrerUser (data) {
-    const response = await fetch(URL+"/singup/", )
+    const response = await fetch(
+        URL+"/singup/",
+        params
+    );
 }
 
-// FETCH
+//----------FETCH----------
+
+// Mokup data.
+/*
+const data = {
+    userData
+}
 
 fetch(
     URL+'/singup/',
     {
         method: 'POST',
-        
+        body: jsonData,
+        headers: {
+            "Content-Type": "application/json"
+        }
     }
 );
+*/
