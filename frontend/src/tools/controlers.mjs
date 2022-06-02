b13e686cda78e1332d21198f92cc5a71a51962f8
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-06-02 09:46:01 +0200

    Moking the fetch

--- frontend/src/tools/controlers.mjs
+++ frontend/src/tools/controlers.mjs
@@ -1,5 +1,15 @@
 import { URL } from "./defines.mjs"
 
 export async function registrerUser (data) {
-    const response = await fetch(URL)
+    const response = await fetch(URL+"/singup/", )
 }
+
+// FETCH
+
+fetch(
+    URL+'/singup/',
+    {
+        method: 'POST',
+        
+    }
+);
