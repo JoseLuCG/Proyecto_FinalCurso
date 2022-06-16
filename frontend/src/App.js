57ac41b6817b33d06a77bfa14b327fb09919e6cc
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-06-16 13:37:03 +0200

    Changes: 	* Install react-router-dom. 	* Building a routes.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,16 +5,17 @@ import ChatView from './Views/ChatView/ChatView';
 import PrincipalView from './Views/PrincipalView/PrincipalView';
 import RegistrerView from './Views/RegistrerView/RegistrerView';
 import StartView from './Views/StartView/StartView';
+import { Link, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <Message/>
-      <OptionsContainer/>
-      <StartView/>
-      <RegistrerView/>
-      <PrincipalView/>
-      <ChatView/>
+      <Routes>
+        <Route path='/login/' element={<StartView/>}/>
+        <Route path='/singup/' element={<RegistrerView/>}/>
+        <Route path='/profiles/' element={<PrincipalView/>}/>
+        <ChatView/>
+      </Routes>
     </>
   );
 }
--- frontend/src/Views/StartView/StartView.jsx
+++ frontend/src/Views/StartView/StartView.jsx
@@ -12,7 +12,7 @@ function StartView () {
     const passwordChangeHandler = changeValueFactory(setPassword);
     return(
         <div className='logingContainer'>
-            <h1>Login view</h1>
+            <h1>Start view</h1>
             <img className='LogoApp' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtepaT1fXQVI9_pyOM34JaAx7aASFWzfmyQg&usqp=CAU" alt="Logo imagen" />
             <input onChange={userInfoChangeHandler} type="text" placeholder='Nombre de usuario o email'/>
             <input onChange={passwordChangeHandler} type="password" placeholder='Contraseña'/>
