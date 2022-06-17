b9c6e26b734e72f224ccf760af59ebac6a0012c7
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-06-17 09:52:14 +0200

    Fixed error of the <react-router-dom>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,18 @@
 import './App.css';
-import Message from './components/Message/Message';
-import OptionsContainer from './components/OptionsContainer/OptionsContainer';
 import ChatView from './Views/ChatView/ChatView';
 import PrincipalView from './Views/PrincipalView/PrincipalView';
 import RegistrerView from './Views/RegistrerView/RegistrerView';
 import StartView from './Views/StartView/StartView';
-import { Link, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path='/login/' element={<StartView/>}/>
-        <Route path='/singup/' element={<RegistrerView/>}/>
+        <Route path='/' element={<StartView/>}/>
+        <Route path='/sing-up/' element={<RegistrerView/>}/>
         <Route path='/profiles/' element={<PrincipalView/>}/>
-        <ChatView/>
+        <Route path='/chat/' element={<ChatView/>}/>
       </Routes>
     </>
   );
--- frontend/src/Views/StartView/StartView.jsx
+++ frontend/src/Views/StartView/StartView.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { changeValueFactory } from '../../tools/apptools.mjs';
+//import { Link } from 'react-router-dom';
 import './StartView.css';
 
 function StartView () {
