c4bf615a1dc449e73cdd8aec70938c49846a75c2
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-06-20 13:04:42 +0200

    Order the code

--- frontend/src/tools/controlers.mjs
+++ frontend/src/tools/controlers.mjs
@@ -2,25 +2,16 @@ import { URL } from "./defines.mjs"
 //----------Constants----------
 
 //----------Functions----------
-/**
- * Realize the fetch at the backend with the user
- * information.
- * @param {Object} data - The user object.
- */
-export async function postUser (data) {
-    const params = dataToString(data)
-    const response = await fetch(
-        URL+"/singup/",
-        params
-    );
-}
+
+//-x-x-x-x-x Generic functions -x-x-x-x-x
+
 /**
  * Transform `dataAtTrasf` in string and create the
  * body of the fetch.
  * @param {Any} dataAtTrasf - The object to transform in json.  
- * @returns 
+ * @returns - The params of the fetch
  */
-function dataToString (dataAtTrasf) {
+ function dataToString (dataAtTrasf) {
     const jsonData = JSON.stringify(dataAtTrasf);
     const params = {
         method: 'POST',
@@ -31,8 +22,37 @@ function dataToString (dataAtTrasf) {
     };
     return params;
 }
+
+
+//-x-x-x-x-x Fetch Functions -x-x-x-x-x
+
+/**
+ * Realize the fetch at the backend with the user
+ * information.
+ * @param {Object} data - The user object.
+ */
+export async function postUser (data) {
+    const params = dataToString(data);
+    const response = await fetch(
+        URL+"/singup/",
+        params
+    );
+}
+/**
+ * Realize the fetch at the backend and get the users in
+ * the database. 
+ * @param {*} url - It is defined in the file 'defines.mjs'. 
+ * @returns The array with the users objects.
+ */
 export async function getUsers (url) {
     const response = await fetch(url+"/users/");
     const data = await response.json();
     return data;
+}
+export async function logingUser (data) {
+    const params = dataToString(data);
+    const response = await fetch(
+        URL+"/login/",
+        params
+    ); 
 }
