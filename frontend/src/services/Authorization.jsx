f861a0dae1101fa92a3ed91198205af067294f22
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-06-22 12:41:53 +0200

    IDFI

--- frontend/src/services/Authorization.jsx
+++ frontend/src/services/Authorization.jsx
@@ -1,11 +1,18 @@
 import { Context } from "./SharedStorage";
 import { useContext } from "react";
 import Login from "../components/Login/Login.jsx";
+import { useEffect } from "react";
 
 function Authorization ({children}) {
     //----------States----------
     const [ store ] = useContext(Context);
 
+    useEffect(
+        ()=>{
+            console.log("Este es el store:",store);
+        },
+        [store]
+    );
     return(
         <>
         { store.id ? children : <Login/> }
