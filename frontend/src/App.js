101975b0366a9a69afe3d6f4c76f050c0a333378
JoseLuis <joseluisconde@pretonmail.com>
2022-06-23 19:34:08 +0200

    Create the <EditingView>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import PrincipalView from './Views/PrincipalView/PrincipalView';
 import RegistrerView from './Views/RegistrerView/RegistrerView';
+import EditingView from './Views/EditingView/EditingView';
 import Login from './components/Login/Login';
 import { Route, Routes } from 'react-router-dom';
 import Authorization from './services/Authorization';
@@ -12,6 +13,7 @@ function App() {
         <Route path='/profiles/' element={<Authorization><PrincipalView/></Authorization>}/>
         <Route path='/' element={<Login/>}/>
         <Route path='/sing-up/' element={<RegistrerView/>}/>
+        <Route path='/edit-user/' element={<EditingView/>}/>
       </Routes>
     </>
   );
