ace7f10a268ba09a10cce1824479966cec854fb6
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-06-29 13:16:02 +0200

    Giving form to the <CircleButton>

--- frontend/src/components/CircleButton/CircleButton.jsx
+++ frontend/src/components/CircleButton/CircleButton.jsx
@@ -1,9 +1,14 @@
 import './CircleButton.css';
-
-function CircleButton () {
+/*
+El icono del boton lo pasa el padre,
+la acción tambien la pasa el padre:
+    *Para el back usaremos:
+        - https://bobbyhadz.com/blog/react-router-go-back-to-previous-page
+*/
+function CircleButton ({imgSrc, action}) {
     return(
-        <button id='circleButton'>
-            back
+        <button id='circleButton' onClick={action}>
+            <img src={imgSrc}/>
         </button>
     );
 }
