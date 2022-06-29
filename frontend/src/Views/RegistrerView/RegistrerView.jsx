f9d1fa036fc795e9e61aa9b21d3a2b0a736aa024
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-06-29 12:56:07 +0200

    Adds the button to profileview

--- frontend/src/Views/RegistrerView/RegistrerView.jsx
+++ frontend/src/Views/RegistrerView/RegistrerView.jsx
@@ -1,18 +1,22 @@
 import CircleButton from '../../components/CircleButton/CircleButton';
 import './RegistrerView.css';
 import ProfileCard from '../../components/ProfileCard/ProfileCard.jsx';
+import { Link } from 'react-router-dom';
 
 function RegistrerView() {
     //----------States:----------
  
     //----------Handlers:----------
 
-    return(
+    return (
         <div className='registrerContainer'>
             <h1>This is the RegistrerView</h1>
-            <CircleButton/>
+            <CircleButton />
             <h1>Completa tus datos:</h1>
-           <ProfileCard/>
+            <ProfileCard/>
+            <Link to={"/profiles/"}>
+                <button >Entrar</button>
+            </Link>
         </div>
     );
 }
