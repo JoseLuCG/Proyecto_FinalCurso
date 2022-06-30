04fec6988c6bd336e0d637df82554ed941f38c36
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-06-30 09:31:35 +0200

    Change the URL

--- frontend/src/tools/defines.mjs
+++ frontend/src/tools/defines.mjs
@@ -1,1 +1,15 @@
-export const URL = 'http://localhost:4000';
+export let URL; 
+
+switch (window.location.hostname) {
+    case "localhost":
+      URL="http://localhost:4000"
+      break;
+
+    case "127.0.0.1":
+      URL="http://127.0.0.1:4000"
+      break;
+      
+    default:
+      URL="/"
+      break;
+}
