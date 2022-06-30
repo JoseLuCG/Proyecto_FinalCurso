4edae1ecbf38a2c0890286435d9a2d706493a4f1
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-06-30 09:53:49 +0200

    Change the PORT constant

--- backend/src/models/defines.mjs
+++ backend/src/models/defines.mjs
@@ -1,4 +1,4 @@
-export const  PORT = 4000;
+//export const  PORT = 4000;
 export const PATH_PREFIX = "/api/v0.0";
 //----------Querys SQL----------
 export const sqlIDReturn = 'SELECT last_insert_rowid() as id';
