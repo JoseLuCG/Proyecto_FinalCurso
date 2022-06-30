048498a758b26562df2f8d807cc6c4429d530e7f
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-06-30 12:03:41 +0200

    Add the icon at the button

--- frontend/src/components/NavigationBar/NavigationBar.jsx
+++ frontend/src/components/NavigationBar/NavigationBar.jsx
@@ -1,10 +1,12 @@
 import CircleButton from '../../components/CircleButton/CircleButton';
+import op_icon from '../../iconos_app/options_icon.jpg';
+import fil_icon from '../../iconos_app/filtrer_icon.jpg';
 
 function NavigationBar() {
     return (
         <div>
-            <CircleButton/>
-            <CircleButton/>
+            <CircleButton imgSrc={op_icon}/>
+            <CircleButton imgSrc={fil_icon}/>
         </div>
     );
 }
