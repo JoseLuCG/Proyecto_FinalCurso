0f329bb951e96583cf928fbd546ce568314e5592
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-06-30 10:24:06 +0200

    Use the environment variables.

--- backend/src/index.mjs
+++ backend/src/index.mjs
@@ -1,6 +1,11 @@
 import express from "express";
 import { getUsersControler, loginUSerControler, singupControler, putUserControler} from "./controlers/controlers.mjs";
-import { PORT } from "./models/defines.mjs";
+import { config } from "dotenv"
+//import { PORT } from "./models/defines.mjs";
+
+if ( process.env.NODE_ENV != "production" ) {
+    config()
+}
 
 //Create the instances:
 const app = express();
@@ -15,8 +20,8 @@ try{
     //app.delete("/user/:id", jsonParser, deleteUserControler);
     app.get("/users/", getUsersControler);
 
-    app.listen(PORT, ()=>{
-        console.log("Express Running...")
+    app.listen( process.env.PORT, ()=> {
+        console.log(`Listening at ${process.env.PORT}`,"Express Running") 
     });
 
     /*app.use((err, req, res, next)=>{
