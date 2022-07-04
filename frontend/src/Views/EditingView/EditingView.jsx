d25a239f530502ec37a43bcb26ccc5433e4938b7
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-07-04 12:43:39 +0200

    Now the user edit your data with your data in the inputs.

--- frontend/src/Views/EditingView/EditingView.jsx
+++ frontend/src/Views/EditingView/EditingView.jsx
@@ -2,10 +2,12 @@ import CircleButton from '../../components/CircleButton/CircleButton';
 import './EditingView.css';
 import ProfileCard from '../../components/ProfileCard/ProfileCard.jsx';
 import back from '../../iconos_app/back_icon.jpg';
+import { useContext } from "react";
+import { Context } from '../../services/SharedStorage';
 
 function EditingView() {
     //----------States:----------
- 
+    const [ store ] = useContext(Context);
     //----------Handlers:----------
 
     return(
@@ -13,7 +15,7 @@ function EditingView() {
             <h1>This is the EditingView</h1>
             <CircleButton imgSrc={back} path={-1}/>
             <h1>Edita tus datos:</h1>
-           <ProfileCard/>
+           <ProfileCard user={store}/>
         </div>
     );
 }
