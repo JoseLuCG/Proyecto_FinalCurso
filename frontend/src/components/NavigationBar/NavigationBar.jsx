af22884b0bdb427537650f2d98b9a832a989b8be
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-07-04 10:12:24 +0200

    Adds the props in the NavigationBar

--- frontend/src/components/NavigationBar/NavigationBar.jsx
+++ frontend/src/components/NavigationBar/NavigationBar.jsx
@@ -2,11 +2,12 @@ import CircleButton from '../../components/CircleButton/CircleButton';
 import op_icon from '../../iconos_app/options_icon.jpg';
 import fil_icon from '../../iconos_app/filtrer_icon.jpg';
 
-function NavigationBar() {
+
+function NavigationBar({actionL, actionR, }) {
     return (
         <div>
-            <CircleButton imgSrc={op_icon}/>
-            <CircleButton imgSrc={fil_icon}/>
+            <CircleButton imgSrc={op_icon} action={actionL}/>
+            <CircleButton imgSrc={fil_icon} action={actionR}/>
         </div>
     );
 }
