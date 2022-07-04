3721dfd2292c300274d3a781b2cbbf36d820b085
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-07-04 12:03:37 +0200

    Change the icon of the button

--- frontend/src/components/OptionsContainer/OptionsContainer.jsx
+++ frontend/src/components/OptionsContainer/OptionsContainer.jsx
@@ -1,13 +1,14 @@
 import CircleButton from '../CircleButton/CircleButton';
 import './OptionsContainer.css';
+import back from '../../iconos_app/back_icon.jpg'
 
 function OptionsContainer () {
     return(
         <div className='optionsContainer'>
             <p>this is a component</p>
-            <CircleButton/>
-            <button>Opcion 1</button>
-            <button>Opcion 2</button>
+            <CircleButton imgSrc={back} path/>
+            <button>Perfil</button>
+            <button>Opciones</button>
         </div>
     );
 }
