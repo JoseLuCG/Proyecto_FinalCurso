dde7d89be5ff07d57ec1846debeb9c49f5b6ad5c
Jose Luis <condegomesjoseluis@gmail.com>
2023-11-15 08:45:55 +0100

    Add the slider menu

--- frontend/src/components/NavigationBar/NavigationBar.jsx
+++ frontend/src/components/NavigationBar/NavigationBar.jsx
@@ -1,10 +1,12 @@
 import './NavigationBar.css'
 import CircleButton from '../../components/CircleButton/CircleButton';
+import SliderMenu from '../SliderMenu/SliderMenu';
 
 function NavigationBar({pathL, pathR, imgL, imgR }) {
     return (
         <div className='toolBar'>
             <CircleButton imgSrc={imgL} path={pathL}/>
+            <SliderMenu/>
             <CircleButton imgSrc={imgR} path={pathR}/>
         </div>
     );
