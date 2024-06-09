2fe46294c91a9933d43c7a7cdf700783c80375f3
JoseLuis <joseluisconde@pretonmail.com>
2024-06-09 23:06:16 +0200

    Create the 'singUpUser' controler that insert a new user in database

--- backend/src/controlers/controlersMySql.mjs
+++ backend/src/controlers/controlersMySql.mjs
@@ -1,2 +1,27 @@
 import { mySqlConn } from "../connection/connection.mjs";
 
+function singUpUser(req, res) {
+/* insertUser (
+	p_profileName VARCHAR(50), p_userName VARCHAR(50),p_password VARCHAR(50),p_location VARCHAR(50),
+    p_age INT,p_description VARCHAR(1000),p_email VARCHAR(50)) */
+
+    const { nameProfile,
+        nameUser,
+        password,
+        location,
+        age,
+        description,
+        email,
+        interest
+    } = req.body;
+    let sql = `CALL insertUser("${nameProfile}", "${nameUser}", "${password}", "${location}", ${age}, "${description}", "${email}")`;
+    mySqlConn.query(sql, function (err) {
+      if (err) console.log(err);
+      else console.log("Correcto");
+      res.send();
+    });
+};
+
+export {
+    singUpUser
+};
