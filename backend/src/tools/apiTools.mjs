2d53d5c6efbe1fb41014fbed903706c92682bc66
JoseLuis <joseluisconde@pretonmail.com>
2024-06-10 04:00:41 +0200

    Add a middleware to insert interest, still don't work

--- backend/src/tools/apiTools.mjs
+++ backend/src/tools/apiTools.mjs
@@ -1,7 +1,37 @@
 import { mySqlConn } from "../connection/connection.mjs";
+import { sqlIDReturn } from "./defines.mjs";
 
-function insertInterests (interests) {
-    console.log(interests);
+function insertInterests (interests, idUser) {
+    interests.forEach(element => {
+        console.log(element);
+        mySqlConn.query(`CALL insertInterest("${element}");`, 
+            (error) => {
+                if (error) {
+                    console.log(error);
+                } else {
+                    mySqlConn.end();
+                    // ---------- Errores a partir de aquí: ----------
+                    mySqlConn.query(sqlIDReturn, function (err, result) {
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            let idinterest = resultId[0].idUser;
+
+                            mySqlConn.query(`CALL insertInterest_user(${idUser}, ${idinterest});`, (errorUI) => {
+                                if (errorUI) {
+                                    console.log(errorUI);
+                                } else {
+                                    console.log("Intereses guardados");
+                                }
+                            });
+                           //console.log("Funciona no toques.");    
+                        }
+                    });
+                    // ---------- ERRORS END  ----------
+                }
+            }
+        );
+    });
 }
 
 export {
