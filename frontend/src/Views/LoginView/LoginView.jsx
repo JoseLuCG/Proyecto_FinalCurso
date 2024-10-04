3f7b06b6e4a4ac14617d210f3f489d0051a34a33
Jose Luis <joseluisconde@protonmail.com>
2024-10-04 10:24:47 +0200

    refactor restructure the login view

---
+++ frontend/src/Views/LoginView/LoginView.jsx
@@ -0,0 +1,16 @@
+import Login from '../../components/Login/Login';
+import './LoginView.css';
+
+
+function LoginView() {
+    //----------States:----------
+ 
+    //----------Handlers:----------
+
+    return (
+        <main>
+            <Login/>
+        </main>
+    );
+}
+export default LoginView;
