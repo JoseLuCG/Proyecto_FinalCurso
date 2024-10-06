efd701160a154eb8245ac00e5eb49ab8c37f37f9
Jose Luis <condegomesjoseluis@gmail.com>
2024-10-06 14:53:26 +0200

    Style(SliderMenu): update HTML classes and add circular button
    Updated several HTML elements with new class names for better stylig
    consistency. Added a circular button to the 'opciones' list item.
    
    - Change class names for all component.
    - Added a new `Circular button` commponent for user options.

--- frontend/src/components/SliderMenu/SliderMenu.jsx
+++ frontend/src/components/SliderMenu/SliderMenu.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './SliderMenu.css';
 import settings from '../../assets/icons/settings.png';
+import prfileOptions from '../../assets/icons/user.png';
 import CircleButton from '../CircleButton/CircleButton';
 import op_icon from '../../assets/icons/options_icon.jpg';
 
@@ -15,19 +16,22 @@ function SliderMenu () {
     }
 
     return(
-        <div className='wrapper'>
-            <button className='menuButton' onClick={handlerMenuState}>
-               
-            </button>
-            <ul className='menuList' hidden={deploy && "hidden"}>
-                <li className='menuItem'>
-                    <button>Opciones</button>
-                </li>
-                <li className='menuItem'>
-                    <CircleButton imgSrc={settings} path={'/edit-user/'}/>
-                    Perfil
-                </li>
-            </ul>
+        <div className='wpr'>
+            <button className='mn-btn' onClick={handlerMenuState}></button>
+            
+            {/* ----- Hidden content ----- */}
+            <nav className='nv-cnt' hidden={deploy && "hidden"}>
+                <ul className='nv-ul'>
+                    <li className='mn-itm'>
+                        <CircleButton imgSrc={settings}/>
+                        Opciones
+                    </li>
+                    <li className='mn-itm'>
+                        <CircleButton imgSrc={prfileOptions} path={'/edit-user/'}/>
+                        Perfil
+                    </li>
+                </ul>
+            </nav>
         </div>
     );
 }
