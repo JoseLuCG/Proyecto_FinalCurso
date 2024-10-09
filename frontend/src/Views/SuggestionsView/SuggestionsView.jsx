32b86b1c9503f91f60d948e8a56b77bdfc57f9a3
Jose Luis <condegomesjoseluis@gmail.com>
2024-10-09 23:26:22 +0200

    Feat(SuggestionsView): add a form and improve page styles
    Added a form for the suggest of the user and add the basic styles for
    the page.

--- frontend/src/Views/SuggestionsView/SuggestionsView.jsx
+++ frontend/src/Views/SuggestionsView/SuggestionsView.jsx
@@ -6,8 +6,37 @@ function SuggestionsView() {
     return(
         <>
             <Header></Header>
-            <main>
-                <h2>Esta es la zona de sugerencias</h2>
+            <main className='mn-cont'>
+                <section className='cont-form'>
+                    <h2>Envía tu sugerencia</h2>
+                    <form action="">
+                        <fieldset>
+                            <legend>Datos personales</legend>
+                            <p>
+                                <label htmlFor="name">Nombre: </label>
+                                <input type="text" id='name' />
+                            </p>
+                            <p>
+                                <label htmlFor="surname">Apellidos: </label>
+                                <input type="text" id="surname" />
+                            </p>
+                            <p>
+                                <label htmlFor="phone">Teléfono: </label>
+                                <input type="text" id="phone" />
+                            </p>
+                            <p>
+                                <label htmlFor="email">Correo electrónico: </label>
+                                <input type="email" id="email" />
+                            </p>
+                            <p>
+                            <label htmlFor="message">Escribe tu sugerencia: </label><br />
+                            <textarea name="" id="message" cols="40" rows="5"></textarea>
+                            </p>
+                        </fieldset>
+                        <button type='submit'>Enviar sugerencia</button>
+                        <button type='reset'>Borrar formulario</button>
+                    </form>
+                </section>
             </main>
         </>
     );
