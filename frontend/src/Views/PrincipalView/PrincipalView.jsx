2da7b060a73de6efa46ff94d57aa3f0e3c73ae71
Jose Luis <condegomesjoseluis@gmail.com>
2024-10-18 15:39:17 +0200

    Feat(PrincipalView): refactor the code
    Changed the logic of the component dividing the component functions of
    the handler functions for more working capacity.

--- frontend/src/Views/PrincipalView/PrincipalView.jsx
+++ frontend/src/Views/PrincipalView/PrincipalView.jsx
@@ -1,36 +1,33 @@
 import './PrincipalView.css';
 import Header from '../../components/Header/Header';
 import ProfileCard from "../../components/ProfileCard/ProfileCard";
-import { useState } from 'react';
-import { getUsers } from '../../tools/controlers.mjs';
+import { useState, useContext } from 'react';
+import { getUsers, getInterests } from '../../tools/connectors/conections.mjs';
 import { URL } from '../../tools/defines.mjs';
 import NavigationBar from '../../components/NavigationBar/NavigationBar';
 import op_icon from '../../assets/icons/options_icon.jpg';
 import fil_icon from '../../assets/icons/filtrer_icon.jpg';
+import { Users } from '../../services/UsersStorage';
 
 function PrincipalView () {
-    const [ users, setUSers ] = useState([]);
-    //----------Handlers----------
-    
-    async function loadUsers () {
-        const a = await getUsers(URL);
-        setUSers(a);
-        setInterest();
-        console.log(users);
+    const [ users, setUsers ] = useContext(Users);
+
+    // ---------- Functions ----------
+    async function loadUserData() {
+        const backendUsers = await getUsers(URL);
+        const backendInterest = await getInterests(URL);
+
+        
+        
+        
     }
 
-    function setInterest() {
-        const usersWithInterest = [];
-        
-        usersWithInterest = users.map(
-            (u) => {
-                Object.defineProperty(u, "interest", {
-                    value: ["Hola", "Mundo"]
-                });
-            }
-        )
-
-        setUSers(usersWithInterest);
+    // ---------- Handlers ----------
+    
+    async function loadUsersHandler () {
+        loadUserData();
+        const backendUsers = await getUsers(URL);
+        setUsers(backendUsers);
     }
 
     return(
@@ -38,7 +35,7 @@ function PrincipalView () {
             <Header></Header>
             <main className="mn-bd">
                 <NavigationBar pathL={'/settings/'} pathR={'/filter/'} imgL={op_icon} imgR={fil_icon}/>
-                <button className='buttonUsersLoader' onClick={loadUsers}>Carga usuarios</button>
+                <button className='buttonUsersLoader' onClick={loadUsersHandler}>Carga usuarios</button>
                 <div className='usersContainer'>
                     {users.map(
                         (user) => <ProfileCard key={user._id} user={user} editable="false" />
