cd99d9dbd4860585facf1134ad21bda22f4d800b
Jose Luis <condegomesjoseluis@gmail.com>
2024-10-23 18:28:37 +0200

    Feat(ProfileCard): adds the use of useEffect and change the operation
    Changed the component operation. Structured the handlers
    in a more effective way. Switch the div main container for a form
    container.

--- frontend/src/components/ProfileCard/ProfileCard.jsx
+++ frontend/src/components/ProfileCard/ProfileCard.jsx
@@ -1,4 +1,4 @@
-import { useState, useContext } from 'react';
+import { useState, useContext, useEffect } from 'react';
 import { OwnUser } from '../../services/OwnUserStorage.jsx';
 import { changeValueFactory } from '../../tools/apptools.mjs';
 import { postUser } from '../../tools/connectors/conections.mjs';
@@ -37,7 +37,7 @@ function ProfileCard ({user, editable}) {
     /**
      * Builds a user that will be introduced in the context.
      */
-    function userbuilder() {
+    function userBuilder() {
         let user = {...ownUser};
         
         user = {
@@ -60,28 +60,121 @@ function ProfileCard ({user, editable}) {
         } else {
             
         }
-        setOwnUser(user);
+
+        return user;
     }
     
-    async function checkSendData() {
-        const response = await postUser(ownUser);
+    function checkSendData(user) {
+        console.log(user);
     }
     
-    /**
-     * This function save the data of states in the context.
-     */
-    async function dataHandler () {
-        userbuilder();
-        checkSendData();
-    }     
-    
-    async function loadInterests () {
+
+    function loadInterests () {
         if (showInterests) setShowInterests(false);    
         else setShowInterests(true);
     }
 
+    function handleSubmit (event) {
+        event.preventDefault();
+        checkSendData(userBuilder());
+        setOwnUser(userBuilder());    
+    }
+
+    async function sendData() {
+        const response = await postUser(ownUser);
+    }
+
+    useEffect(() => {
+        sendData();
+      }, [ownUser]);
+    
     return (
-        <div className={styles.profileContainer}>
+        <form className={styles.profileContainer} onSubmit={handleSubmit}>
+             <div className={styles.containerPhotoAge}>
+                <PicIcon img="https://s.ws.pho.to/img/index/ai/source.jpg" />
+                <input 
+                  disabled={editable && "disabled"} 
+                  value={user && user.age} 
+                  onChange={ageChangeHandler} 
+                  className={[styles.age, styles.inputData].join(' ')} 
+                  type="number" 
+                  placeholder="Edad" 
+                  min="10" 
+                  max="100"
+                />
+            </div>
+            <div className={styles.dataInputs}>
+                <input 
+                  disabled={editable && "disabled"} 
+                  value={user && user.nameProfile} 
+                  onChange={nameProfileChangeHandler} 
+                  className={styles.inputData} 
+                  type="text" 
+                  placeholder="Nombre de perfil" 
+                />
+                <input 
+                  disabled={editable && "disabled"} 
+                  value={user && user.nameUser} 
+                  onChange={nameChangeHandler} 
+                  className={styles.inputData} 
+                  type="text" 
+                  placeholder="Nombre" 
+                />
+                <input 
+                  disabled={editable && "disabled"} 
+                  value={user && user.location} 
+                  onChange={locationChangeHandler} 
+                  className={styles.inputData} 
+                  type="text" 
+                  placeholder="Ciudad" 
+                />
+                <input 
+                  disabled={editable && "disabled"}
+                  hidden={editable && "hidden"} 
+                  onChange={interestChangeHandler} 
+                  className={styles.inputData}
+                  type="text"
+                  placeholder="Intereses" 
+                />
+                <button 
+                  className={styles.deployInterestsButton} 
+                  onClick={loadInterests}
+                >
+                    display interests
+                </button>
+                <div 
+                  className={styles.interestsContainer} 
+                  hidden={showInterests && "hidden"}
+                >
+                    <ul>
+                        {
+                            user? 
+                                user.interest.map(
+                                    (interest) => { return (<InterestItem interest={interest}/>)}
+                                )
+                                : "None"
+                        }
+                    </ul>
+                </div>
+            </div>
+            <div className={styles.textareaDescription}>
+            <textarea disabled={editable && "disabled"} value={user && user.description} onChange={descriptionChangeHandler} name="description" className={styles.description} cols="20" rows="6" placeholder="Descripción"></textarea>
+            <button className={styles.messageButton}>Mensaje</button>
+            </div>
+            <div hidden={editable && "hidden"} className={styles.userLogin}>
+                <div className={styles.userLogin}>
+                    <input onChange={emailChangeHandler} className={[styles.email, styles.inputData].join(' ')} type="email" placeholder="Correo" />
+                    <input onChange={passwordChangeHandler} className={styles.inputData} type="password" placeholder="Contraseña" />
+                    <button type="submit" className={styles.inputData}>Save</button>
+                </div>
+            </div>
+        </form>
+    );
+}
+export default ProfileCard;
+
+/*
+ <div className={styles.profileContainer}>
             <div className={styles.containerPhotoAge}>
                 <PicIcon img="https://s.ws.pho.to/img/index/ai/source.jpg" />
                 <input 
@@ -161,6 +254,4 @@ function ProfileCard ({user, editable}) {
                 </div>
             </div>
         </div>
-    );
-}
-export default ProfileCard;
+*/
