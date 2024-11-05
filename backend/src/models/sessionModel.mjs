9904d7f13163c9af5e37a4783576e43d8ac3faa7
Jose Luis <condegomesjoseluis@gmail.com>
2024-11-06 00:36:49 +0100

    Feat(models): instance the session table model
    Instantiated a sessions table model.

---
+++ backend/src/models/sessionModel.mjs
@@ -0,0 +1,25 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../connection/sequelizeConn.mjs";
+
+const Session = sequelize.define("Session",{
+    id: {
+        type: DataTypes.STRING(255),
+        primaryKey: true,
+        allowNull: false
+    },
+    data_session: {
+        type: DataTypes.JSON,
+        allowNull:false
+    },
+    expires: {
+        type: DataTypes.DATE,
+        allowNull: false
+    }
+}, {
+    tableName: "sessions",
+    timestamps: false
+});
+
+export {
+    Session
+};
