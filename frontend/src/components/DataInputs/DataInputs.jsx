42078a4b1adca223221f5faa67a3a5934e2d576e
Jose Luis <condegomesjoseluis@gmail.com>
2024-11-05 11:28:14 +0100

    Feat(DataInputs): create the new component for the data inputs

--- frontend/src/components/DataInputs/DataInputs.jsx
+++ frontend/src/components/DataInputs/DataInputs.jsx
@@ -1,7 +1,74 @@
+import { useState } from 'react';
+import InterestItem from '../InterestItem/InterestItem.jsx';
 import './DataInputs.css';
 
-function DataInputs() {
-    
+function DataInputs({user, editable, userForm, userFormHandler}) {
+  const [ showInterests, setShowInterests ] = useState(true);
+
+  function loadInterests() {
+    if (showInterests) setShowInterests(false);
+    else setShowInterests(true);
+  }
+    return(
+        <div className='dataInputs'>
+          <input
+            name='nameProfile'
+            disabled={editable && "disabled"}
+            value={user ? user.nameProfile : userForm.nameProfile}
+            onChange={userFormHandler}
+            className='inputData'
+            type="text"
+            placeholder="Nombre de perfil"
+          />
+          <input
+            name='nameUser'
+            disabled={editable && "disabled"}
+            value={user ? user.nameUser : userForm.nameUser}
+            onChange={userFormHandler}
+            className='inputData'
+            type="text"
+            placeholder="Nombre"
+          />
+          <input
+            name='location'
+            disabled={editable && "disabled"}
+            value={user ? user.location : userForm.location}
+            onChange={userFormHandler}
+            className='inputData'
+            type="text"
+            placeholder="Ciudad"
+          />
+          <input
+            name='interest'
+            disabled={editable && "disabled"}
+            hidden={editable && "hidden"}
+            onChange={userFormHandler}
+            className='inputData'
+            type="text"
+            placeholder="Intereses"
+          />
+          <button
+            className='deployInterestsButton'
+            onClick={loadInterests}
+          >
+            display interests
+          </button>
+          <div
+            className='interestsContainer'
+            hidden={showInterests && "hidden"}
+          >
+            <ul>
+              {
+                user ?
+                  user.interest.map(
+                    (interest) => { return (<InterestItem interest={interest} />) }
+                  )
+                  : "None"
+              }
+            </ul>
+          </div>
+        </div>
+    );
 }
 
 export default DataInputs;
