1723cde7fa0d5b0f8351a5dc94ca7b05f35342c9
DanBaDo <bytes4life@proton.me>
2024-11-06 14:48:59 +0100

    Configures project for using environment variables
    Provides db connection data environment variables

--- backend/src/connection/sequelizeConn.mjs
+++ backend/src/connection/sequelizeConn.mjs
@@ -1,10 +1,15 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize("social","root", "abc123.", {
-    host: "localhost",
-    dialect: "mysql",
-    logging: false
-});
+const sequelize = new Sequelize(
+    process.env.DATABASE_TABLE,
+    process.env.DATABASE_USER,
+    process.env.DATABASE_PASSWORD,
+    {
+        host: process.env.DATABASE_HOST,
+        dialect: "mysql",
+        logging: false
+    }
+);
 
 export {
     sequelize
