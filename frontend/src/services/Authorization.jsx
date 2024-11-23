b8050012a8cb77562c119b6025b89e4e22f5cce2
Jose Luis <condegomesjoseluis@gmail.com>
2024-11-23 11:00:12 +0100

    Feat(Authorization): change the method of granting authorization
    Changed the way of checking the authorization. From checking
    if a user id exists to checking if a session cookie exists.

--- frontend/src/services/Authorization.jsx
+++ frontend/src/services/Authorization.jsx
@@ -6,11 +6,21 @@ function Authorization ({children}) {
     //----------States----------
     //In mongodb the id will be _id. In SQLite is 'id'.
     const [ user ] = useContext(OwnUser);
+    const sessionCookie = document.cookie.split('; ').some(
+        cookie => cookie.startsWith('cookie_session=')
+    );
+
+    console.log(sessionCookie);
+    
 
     return(
         <>
-        { user.id ? children : <LoginView/> }
+        { sessionCookie ? children : <LoginView/> }
         </>
     );
 }
 export default Authorization;
+
+/*
+    { user.id ? children : <LoginView/> }
+*/
