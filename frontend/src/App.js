f862f55497795b166581e5db87d973b6a568ee62
Jose Luis <condegomesjoseluis@gmail.com>
2024-11-30 20:21:37 +0100

    Refactor(App.js): import the sessionCookie

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,8 +10,10 @@ import { Route, Routes } from 'react-router-dom';
 import Authorization from './services/Authorization';
 import OptionsContainer from './components/OptionsContainer/OptionsContainer';
 import SuggestionsView from './Views/SuggestionsView/SuggestionsView';
+import { sessionCookie } from './tools/defines.mjs';
 
 function App() {
+
   return (
     <>
       <Routes>
