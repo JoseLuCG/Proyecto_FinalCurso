290b7deb97ddad3aee724529a1bf6b62e792d0f6
JoseLuCG <joseluisconde@protonmail.com>
2024-12-09 21:10:39 +0000

    Refactor(session connection): delete sessionStore useless methods

--- backend/src/connection/connectionSessionMySQL.mjs
+++ backend/src/connection/connectionSessionMySQL.mjs
@@ -20,43 +20,6 @@ const options = {
 
 const sessionStore = new MySQLStore(options, mySqlConn);
 
-sessionStore.deleteSession = function (sessionId) {
-    const sql = `
-        SELECT *
-        FROM sessions
-        WHERE sid = "${sessionId}";
-    `;
-    mySqlConn.query(sql, (error, result) => {
-        if (error) {
-            console.error(error);
-        } else {
-            let data = result[0];
-
-            return data;
-        }
-    });
-}
-
-
 export {
     sessionStore
 }
-
-/*
-Sesion:
-{
-    "cookie": {
-        "originalMaxAge":300000,
-        "expires":"2024-11-11T12:39:56.798Z",
-        "httpOnly":true,
-        "path":"/"
-        },
-    "userId":0,
-    "nameProfile":"PruebaSesiones",
-    "data_session":"{
-        \"userId\":0,
-        \"name_profile\":\"PruebaSesiones\",
-        \"userType\":\"new User\"
-        }"
-}
-*/
