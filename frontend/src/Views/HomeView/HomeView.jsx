d8a9cf2e20a7a380745201f37a190086c87c73d0
JoseLuCG <joseluisconde@protonmail.com>
2024-12-14 15:34:48 +0100

    Feat(HomeView): create a new component for the home page

---
+++ frontend/src/Views/HomeView/HomeView.jsx
@@ -0,0 +1,13 @@
+import Header from '../../components/Header/Header';
+import './HomeView.css';
+
+function HomeView() {
+    return(
+        <>
+            <Header/>
+            <h1>Esta es la pagina de inicio</h1>
+        </>
+    );
+}
+
+export default HomeView;
