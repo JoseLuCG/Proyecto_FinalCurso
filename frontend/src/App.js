d6edce6803287fff633401ec574cea8b58140900
JoseLuCG <joseluisconde@protonmail.com>
2024-12-14 14:55:00 +0100

    Refactor(app): remove unnecessary imports

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,12 +5,10 @@ import EditingView from './Views/EditingView/EditingView';
 import LoginView from './Views/LoginView/LoginView';
 import AboutView from './Views/AboutView/AboutView';
 import RulesView from './Views/RulesView/RulesView';
-import Header from './components/Header/Header';
 import { Route, Routes } from 'react-router-dom';
 import Authorization from './services/Authorization';
 import OptionsContainer from './components/OptionsContainer/OptionsContainer';
 import SuggestionsView from './Views/SuggestionsView/SuggestionsView';
-import { sessionCookie } from './tools/defines.mjs';
 import { useContext, useEffect } from 'react';
 import { OwnUser } from './services/OwnUserStorage';
 
