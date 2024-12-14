81715b7939590dc37d951145a9ededc8b8941c51
JoseLuCG <joseluisconde@protonmail.com>
2024-12-14 17:01:14 +0100

    Docs(user controllers): delete unnecessary documentation

--- backend/src/controllers/userControllers/userControllersMySql.mjs
+++ backend/src/controllers/userControllers/userControllersMySql.mjs
@@ -23,31 +23,6 @@ async function logingUserControler(request, response) {
         console.error(err);
         response.sendStatus(500);
     }
-    /*
-        (err,result)=>{
-            if (err){
-                console.error(err);
-                res.status(500).json({ message: "Error loging user into database." });
-                
-            } else {
-                let data = result[0];
-                if (data) {
-                    req.session.userId = data.id;
-                    req.session.nameProfile = data.nameProfile;
-                    const sessionData = {
-                        userId : data.id,
-                        name_profile : data.nameProfile,
-                        userType : "user acount"
-                    }
-                    req.session.data_session = JSON.stringify(sessionData);
-                    res.status(201).json(data);
-                    //console.log(req.session);   
-                } else {
-                    res.sendStatus(401).json({ message: "Error. Authorization is required." });
-                }
-            }
-        }
-    */
 }
 
 export {
