b50958d17d265899b5e59b99e1e9d022d069e34f
JoseLuCG <joseluisconde@protonmail.com>
2024-12-14 16:42:44 +0100

    Style(HomeView): add two buttons and improve the style

--- frontend/src/Views/HomeView/HomeView.jsx
+++ frontend/src/Views/HomeView/HomeView.jsx
@@ -8,13 +8,26 @@ function HomeView() {
             <main className='mn-cntnr'>
                 <section className='stSection'>
                     <h1>¡Bienvenido a Bloomly!</h1>
+                    <p>
+                        A veces solo se necesita una buena convesación
+                        para que el día mejore. ¿No sabes con quien hablar?
+                        ¡Regístrate o inicia sesión! Podrás hablar con muchas
+                        personas con las que compartiras aficiones, intereses,
+                        ¿y por qué no?, que estén cerca de tí.
+                    </p>
                 </section>
                 <section className='ndSection'>
-                    <section>
-                        <h2>¿Eres nuevo? ¡Regístrate!</h2>
+                    <section className='ndS-left'>
+                        <div className='l-dv'>
+                            <h2>¿Eres nuevo? ¡Regístrate!</h2>
+                            <button>Registro</button>
+                        </div>
                     </section>
-                    <section>
-                        <h2>¿Tienes una cuenta? ¡Inicia sesión!</h2>
+                    <section className='ndS-right'>
+                        <div className='r-dv'>
+                            <h2>¿Tienes una cuenta? ¡Inicia sesión!</h2>
+                            <button>Inicio Sesión</button>
+                        </div>
                     </section>
                 </section>
             </main>
