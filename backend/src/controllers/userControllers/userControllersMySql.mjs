b9c0599c9de1680845dc1fda24ebd29c72af797b
JoseLuCG <joseluisconde@protonmail.com>
2024-12-14 19:29:17 +0100

    Refactor(user controllers): add the `singUpUser` and add documentation

--- backend/src/controllers/userControllers/userControllersMySql.mjs
+++ backend/src/controllers/userControllers/userControllersMySql.mjs
@@ -1,7 +1,14 @@
 import { getUserData } from "./dataHandlers/getUserDataHandler.mjs";
 import { getInterestData } from "./dataHandlers/getUserInterestHandler.mjs";
+import setUserData from "./dataHandlers/setUserDataHandler.mjs";
+import insertUserInterests from "./dataHandlers/setUserInterests.mjs";
 import { userMolder } from "./tools/userMold.mjs";
 
+/**
+ * Logs the user into the application.
+ * @param {*} request Object represents the HTTP request query string. 
+ * @param {*} response Object represents the HTTP response that an Express app sends when it gets an HTTP request.
+ */
 async function logingUserControler(request, response) {
     const {userData,password} = request.body;
     
@@ -25,6 +32,41 @@ async function logingUserControler(request, response) {
     }
 }
 
+async function singUpUser(request, response) {
+    const {
+        nameProfile,
+        nameUser,
+        password,
+        location,
+        age,
+        description,
+        email,
+        interest
+    } = request.body;
+    const user = {
+        nameProfile: nameProfile,
+        nameUser: nameUser,
+        password: password,
+        location: location,
+        age: age,
+        description: description,
+        email: email,
+    }
+
+    try {
+        const insertedUser = await setUserData(user);
+        const insertedInterests = insertUserInterests(interest, insertedUser.idUser);
+        
+        if (insertedInterests == "OK") {
+            response.sendStatus(200);
+        }
+    } catch (error) {
+        console.error(error);
+        response.sendStatus(500);
+    }
+}
+
 export {
-    logingUserControler
+    logingUserControler,
+    singUpUser
 }
