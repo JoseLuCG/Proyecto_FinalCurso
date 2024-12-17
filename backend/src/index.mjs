d2da039eacf01f5bb9f9a95a52e6afee2c4af53e
JoseLuCG <joseluisconde@protonmail.com>
2024-12-17 21:05:04 +0100

    Feat(index): implements the socket.io in the backend

--- backend/src/index.mjs
+++ backend/src/index.mjs
@@ -1,5 +1,7 @@
 import express from "express";
-import session, { Store } from "express-session";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
+import session from "express-session";
 import connectSessionSequelize from "connect-session-sequelize" ;
 import { sessionStore } from "./connection/connectionSessionMySQL.mjs";
 import cors from 'cors'
@@ -23,6 +25,15 @@ if ( process.env.NODE_ENV != "production" ) {
 
 // ---------- Create the instances of express: ----------
 const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+    cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    credentials: true,
+    transports: ['websocket']
+  }
+});
 const jsonParser = express.json();
 const cookieDuration = 1000 * 60 * 15; // * Will linger 15 minutes
 const SequelizeStore = connectSessionSequelize(session.Store);
@@ -31,7 +42,7 @@ const SequelizeStore = connectSessionSequelize(session.Store);
 // ---------- Endpoints of the API with MySQL: ----------
 
 try{
-    app.use("/",express.static("../frontend/build/", {index: "index.html"}));
+    //app.use("/",express.static("../frontend/build/", {index: "index.html"}));
 
     app.use(cors({
         origin: 'http://localhost:3000',
@@ -54,6 +65,35 @@ try{
             expires: new Date(Date.now() + cookieDuration).toISOString().slice(0,19).replace('T'," ")
     }));
 
+    // ---------- Sockets: ----------
+ 
+    io.engine.on("connection_error", (err) => {
+        console.log(err.req);      // the request object
+        console.log(err.code);     // the error code, for example 1
+        console.log(err.message);  // the error message, for example "Session ID unknown"
+        console.log(err.context);  // some additional error context
+    });
+
+    // Listen for connections from clients
+    io.on('connection', (socket) => {
+        console.log('A user connected');
+
+        // Listen for messages from clients
+        socket.on('sendMessage', (message) => {
+            console.log('Received message:', message);
+
+            // Broadcast the message to the other user
+            // Example: Emit the message to a specific room or user
+            socket.broadcast.emit('receiveMessage', message);
+        });
+
+        // Handle user disconnections
+        socket.on('disconnect', () => {
+            console.log('A user disconnected');
+        });
+    });
+
+
     // ----- User Endpoints -----
     app.post("/singup/",jsonParser, singUpUser);
     app.post("/login/", jsonParser, authorizationMiddleware,logingUserController);
@@ -75,10 +115,9 @@ try{
     //---------- Listen the port ----------
     sessionStore.onReady().then(()=> {
         console.log("MySQLStore ready.");
-        app.listen(process.env.PORT, () => {
+        server.listen(process.env.PORT, () => {
             console.log(`Listening at ${process.env.PORT}`,"Express Running");
-            
-        });  
+        }); 
     }).catch((error)=> {
         console.error('Error syncing the sessions table',error);
         
