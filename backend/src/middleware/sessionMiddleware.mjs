4eb99f6ff97e2e124a6c5d1c8c317493f129327a
JoseLuCG <joseluisconde@protonmail.com>
2024-12-27 15:34:36 +0100

    Fix(sessionMiddleware): imports problem solved
    Solved the imports problem. Adding them in the file.

--- backend/src/middleware/sessionMiddleware.mjs
+++ backend/src/middleware/sessionMiddleware.mjs
@@ -1,3 +1,12 @@
+import session from "express-session";
+import { sessionStore } from "./../connection/connectionSessionMySQL.mjs";
+import { config } from "dotenv";
+
+if ( process.env.NODE_ENV != "production" ) {
+    config()
+}
+const cookieDuration = 1000 * 60 * 15; // * Will linger 15 minutes
+
 const sessionMiddleware = session({
     key: "cookie_session",
     secret: process.env.SECRET,
