9fffea328a4ebfec5137690142f612e3b7380763
JoseLuCG <joseluisconde@protonmail.com>
2024-12-27 18:24:18 +0100

    feat(In progress): implement socket.io in the backend

--- backend/src/connection/socketConnection.mjs
+++ backend/src/connection/socketConnection.mjs
@@ -1,5 +1,6 @@
 import { Server } from 'socket.io';
 import sessionMiddleware from '../middleware/sessionMiddleware.mjs';
+import getMessagesData from '../controllers/messagesControllers/dataHandlers/getMessagesDataHandler.mjs';
 
 let io;
 
@@ -13,4 +14,21 @@ export function initSocket(server) {
     });
     io.engine.use(sessionMiddleware);
 
+    io.on("connection", (socket) => {
+        console.log(`User connected: ${socket.id}`);
+
+        socket.on("fetch-messages", async ( idEmisor, idReceptor ) => {
+            try {
+                const messages = await getMessagesData(idEmisor, idReceptor);
+                socket.emit("messages-data", messages);
+            } catch (error) {
+                console.error("Error fetching messages:", error);
+                socket.emit("fetch-error", { error: "Unable to fetch messages" });
+            }
+        });
+
+        socket.on("disconnect", ()=> {
+            console.log(`User disconnected: ${socket.id}`);
+        });
+    });
 } 
