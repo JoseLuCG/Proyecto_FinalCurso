d080e4e10f2acf4f84b2a1078d7ac4c1c19c25eb
Jose Luis <condegomesjoseluis@gmail.com>
2025-02-03 11:39:22 +0100

    Feat(backend handlers): change the table and add `messageTime` parameter

--- backend/src/controllers/messagesControllers/dataHandlers/setMessageDataHandler.mjs
+++ backend/src/controllers/messagesControllers/dataHandlers/setMessageDataHandler.mjs
@@ -1,9 +1,9 @@
 import { mySqlConn } from "../../../connection/connection.mjs";
 
-function setNewMessageHandler(idUserEmisor, idUserReceptor, message_body) {
+function setNewMessageHandler(messageTime, idUserEmisor, idUserReceptor, message_body) {
     const sqlQuery = `
-        INSERT INTO message_proves_v1 (id_emisor_user, id_receptor_user, message_body)
-            VALUES (${idUserEmisor},${idUserReceptor},"${message_body}");
+        INSERT INTO message_proves_v2 (messageTime, id_emisor_user, id_receptor_user, message_body)
+            VALUES ("${messageTime}", ${idUserEmisor},${idUserReceptor},"${message_body}");
     `;
 
     return new Promise(
