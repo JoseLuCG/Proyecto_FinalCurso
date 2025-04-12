ef3026e862b5f9e82eb365a0ca7f797594225978
Jose Luis <condegomesjoseluis@gmail.com>
2025-04-12 22:56:39 +0200

    Feat(component): add the component logic and add styles
    Added the component logic and style to text input style.

--- frontend/src/components/Card/Card.jsx
+++ frontend/src/components/Card/Card.jsx
@@ -1,14 +1,64 @@
 import './Card.css';
 import bloomlyLogo from '../../assets/Bloomly_logo.png';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useContext, useState, useEffect, useRef } from 'react';
+import { OwnUser } from '../../services/OwnUserStorage';
+import { logingUser } from '../../tools/connectors/conections.mjs';
+import { messsage } from '../../tools/defines.mjs';
 
 function Card({ isHomeView, setIsHomeView }) {
     // States:
-    
+    const [ ownUser, setOwnUser ] = useContext(OwnUser);
+    const [ user, setUser ] = useState({
+        emailOrProfile: "",
+        password: ""
+    });
+    const prevUserRef = useRef(user);
+    const navigate = useNavigate();
 
+    //Handlers:
     function setIsHomeViewHandler() {
         setIsHomeView(false);
     }
+
+    function setUserHandler(event) {
+        const { name, value } = event.target;
+
+        setUser(previousState => ({
+            ...previousState,
+            [name]: value
+        }));
+    }
+
+    function setLocalSotore() {
+        localStorage.setItem("User", JSON.stringify(user));
+    }
+    
+    async function sendLogin(event) {
+        try {
+            event.preventDefault();
+            const response = await logingUser(user);
+            setUser(response);
+            setLocalSotore();
+            navigate("/profiles/");
+            window.location.reload();
+        } catch (error) {
+            alert(messsage.sessionError)
+        }
+    }
+
+    function navigateToSingUp () {
+        navigate("/sing-up/");
+    }
+
+    useEffect(() => {
+        if ((prevUserRef.current !== user) && user.id) {
+            setLocalSotore();
+            prevUserRef.current = user;
+        }
+    },[user]
+    );
+
     return (
         <>
             {
@@ -17,19 +67,31 @@ function Card({ isHomeView, setIsHomeView }) {
                         <img src={bloomlyLogo} alt="Bloomly Logo" class="logo" />
                         <h1>Bienvenido a Bloomly</h1>
                         <p>Conecta, comparte y florece con personas afines.</p>
-                        <button class="btn" onclick="{}">Registrarse</button>
+                        <button class="btn" onClick={navigateToSingUp}>Registrarse</button>
                         <button class="btn" onClick={setIsHomeViewHandler}>Iniciar sesión</button>
                     </div>
                     :
                     <div class="form-container">
                         <h2>Iniciar sesión</h2>
-                        <form action={setIsHomeView} method="POST">
-                            <input type="email" name="email" placeholder="Correo electrónico" required/>
-                            <input type="password" name="password" placeholder="Contraseña" required/>
+                        <form action={setIsHomeView} onSubmit={sendLogin}>
+                            <input 
+                            type="text" 
+                            placeholder="Correo electrónico o nombre de perfil"
+                            name="emailOrProfile"
+                            value={user.emailOrProfile}
+                            required
+                            onChange={setUserHandler}/>
+                            <input 
+                            type="password" 
+                            placeholder="Contraseña"
+                            name="password"
+                            value={user.password}
+                            required
+                            onChange={setUserHandler}/>
                             <input type="submit" value="Entrar"/>
                         </form>
                         <div class="extra">
-                            ¿No tienes cuenta? <a href="registro.html">Regístrate aquí</a>
+                            ¿No tienes cuenta? <Link to={"/sing-up/"}>Regístrate aquí</Link>
                         </div>
                     </div>
             }
